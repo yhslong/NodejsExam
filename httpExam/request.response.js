a68a0540a2434e370d2b680276c2677108007e50
unknown <yhslong@naver.com>
2017-09-23 16:39:56 +0900

    Ejs, JADE 추가

---
+++ httpExam/request.response.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var fs = require('fs');
+var ejs = require('ejs');
+var jade = require('jade');
+
+http.createServer(function(req,res){
+    if(req.method == 'GET'){
+    	console.log(req.url+"GET");
+    	if(req.url == '/'){
+          fs.readFile('node.html',function(err,data){
+    		if(!err){
+    			res.writeHead(200,{'Content-Type':'text/html'});
+    			res.end(data);
+    		} else {
+    			res.writeHead(404);
+    			res.end();
+    		}
+    	   });
+    	} else if(req.url == '/ejs')
+    	{
+          fs.readFile('template.ejs','utf8',function(err,data){
+          	if(!err){
+          		var html = ejs.render(data,
+          			{name:'Hong',description:"Hello,World for Ejs"});
+          		res.writeHead(200,{'Content-Type':'text/html'});
+          		res.end(html);
+          	}
+          });
+    	} else if(req.url == '/jade'){
+    		fs.readFile('template.jade','utf8',function(err,data){
+    			if(!err){
+    				var fn = jade.compile(data);
+    			    var html = fn({name:'Hong',description:"Hello,World for JADE"});
+          		    res.writeHead(200,{'Content-Type':'text/html'});
+          		    res.end(html);
+    			}
+    		})
+    	}
+    	
+    } else if(req.method == 'POST'){
+    	console.log(req.url+"POST");
+    	req.on('data',function(data){
+    		res.writeHead(200,{'Content-Type':'text/html'});
+    		res.end('<h1>'+data+'</h1>');
+    	});
+    }
+
+}).listen(52273,function(){
+	console.log('Server running.....')
+});
--- node.server.js
+++ node.server.js
@@ -4,4 +4,4 @@ http.createServer(function(req,res){
 	res.end('<h1>Hello,World</h1>');
 }).listen(52273,function(){
 	console.log('Server running at http://127.0.0.1:52273/');
-})
+});
